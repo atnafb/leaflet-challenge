da1506b1c5baea9b48cbf6a542ef95133f2ffbee
atnafb <atnafub.ayalew@gmail.com>
2025-03-13 11:36:37 -0400

    adding satellite and topographic map layers

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -12,16 +12,32 @@ var grayscale =  L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner/{z
 	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	ext: 'png'
 });
+
+// Adding salellite layer 
+var satellite = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'jpg'
+});
+
+// adding topographic layer 
+var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
 // Make a basement Object
 let basemaps = {
-  Default: defaultMap,
-  GrayScale: grayscale
-}
+  "Default": defaultMap,
+  "GrayScale": grayscale, 
+  "Satellite Map": satellite,
+  "Topographic Map": OpenTopoMap
+};
 // Create the map object with center and zoom options.
 var myMap = L.map("map", {
   center: [36.7783, -119.4179],
   zoom: 3,
-  layers: [defaultMap, grayscale]
+  layers: [defaultMap]
 });
 
 // Then add the 'basemap' tile layer to the map.
