abe879ea5acf92666751809e495d268511caf09d
atnafb <atnafub.ayalew@gmail.com>
2025-03-13 17:19:09 -0400

    load USGS last 7 days data

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -69,8 +69,12 @@ tectonicplates.addTo(myMap)
 
 // create a variable to store the earthquake data layer 
 let earthquakes = new L.layerGroup();
-
-
+// Make a request that retrieves the earthquake geoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson")
+.then(
+  function (earthquakeData){
+  console.log(earthquakeData);
+});
 // Add a control to the map that will allow the user to change which layers are visible.
 let overlays = {
   "Tectonic Palates": tectonicplates
